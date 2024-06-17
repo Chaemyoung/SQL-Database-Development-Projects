1594d29d35aa15c8b7443cce52a05e722493a059
Chaemyoung <chaemyoung2015@gmail.com>
2024-06-16 23:21:23 -0700

    Write ALTER Syntax to change the table definition

---
+++ SSMS Management System/01.OnlineSales/04.UPDATE syntax - To change data in the table.sql
@@ -0,0 +1,8 @@
+-- Change the MembershipStartDate to November 01 2022 for Mike Johnson in Customer table.
+UPDATE Customer
+SET MembershipStartDate = '2022-11-01'
+WHERE CustomerId = 1
+
+-- In the Customer table set the value for the column EmailAddress to 'Not Provided' for all the records.
+UPDATE Customer
+SET EmailAddress = 'NotProvided'
