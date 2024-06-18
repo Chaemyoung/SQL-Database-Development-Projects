4b6e228585070003101835bdaa8973deafb4488c
Chaemyoung <chaemyoung2015@gmail.com>
2024-06-17 22:02:46 -0700

    Complete Tasks 1,3,6

--- SSMS Management System/02.Library Management/02.Library_Management_Tasks.sql
+++ SSMS Management System/02.Library Management/02.Library_Management_Tasks.sql
@@ -1,10 +1,20 @@
 /* Find Most Borrowed Books. */
-
+SELECT b.BookTitle, COUNT(br.BorrowId) AS BorrowCount
+FROM Books b
+JOIN Borrows br ON b.BookId = br.BookId
+GROUP BY b.BookTitle
+ORDER BY BorrowCount DESC
 
 /* Identify all active and inactive staff members. */
 
+SELECT * FROM Borrows
 
 /* List all books that are currently overdue for return. */
+SELECT c.CusFirstName, c.CusLastName, b.BookTitle, br.DateNeedToReturn
+FROM Customers c
+JOIN Borrows br ON c.CustomerId = br.CustomerId
+JOIN Books b ON br.BookId = b.BookId
+WHERE br.DateNeedToReturn < GETDATE() AND br.BorrowId NOT IN (SELECT BorrowId FROM Returns);
 
 
 /* Generate a borrowing history for a specific customer. */
@@ -14,7 +24,10 @@
 
 
 /* Analyze borrowing trends over the past year. */
-
+SELECT MONTH(br.BorrowDate) AS Months, COUNT(br.BorrowId) AS BorrowCount
+FROM Borrows br
+GROUP BY DATEPART(MONTH, BorrowDate)
+ORDER BY BorrowCount
 
 /* Evaluate the performance of staff based on the number of books issued and returned under their supervision. */
 
