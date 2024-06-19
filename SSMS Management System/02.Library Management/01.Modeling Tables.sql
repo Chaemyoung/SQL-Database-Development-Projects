1aff8349e77137061d2530332f43d274666b7faa
Chaemyoung <chaemyoung2015@gmail.com>
2024-06-19 00:44:35 -0700

    Update by create FinePayments table

--- SSMS Management System/02.Library Management/01.Modeling Tables.sql
+++ SSMS Management System/02.Library Management/01.Modeling Tables.sql
@@ -8,25 +8,25 @@ CREATE TABLE Authors (
     AuthorId BIGINT PRIMARY KEY,
     AuthorFirstName VARCHAR(255),
     AuthorLastName VARCHAR(255)
-);
+)
 
 -- BooksStatus Table
 CREATE TABLE BooksStatus (
     BookStatusId VARCHAR(255) PRIMARY KEY,
     Status VARCHAR(255)
-);
+)
 
 -- Sex Table
 CREATE TABLE Sex (
     SexTypeId VARCHAR(255) PRIMARY KEY,
     SexName VARCHAR(255)
-);
+)
 
 -- StaffStatus Table
 CREATE TABLE StaffStatus (
     StaffStatusId VARCHAR(255) PRIMARY KEY,
     Status VARCHAR(255)
-);
+)
 
 -- Staffs Table
 CREATE TABLE Staffs (
@@ -39,13 +39,13 @@ CREATE TABLE Staffs (
     StaffStatusId VARCHAR(255),
     FOREIGN KEY (SexTypeId) REFERENCES Sex(SexTypeId),
     FOREIGN KEY (StaffStatusId) REFERENCES StaffStatus(StaffStatusId)
-);
+)
 
 -- CustomerStatus Table
 CREATE TABLE CustomerStatus (
     CusStatusId VARCHAR(255) PRIMARY KEY,
     Status VARCHAR(255)
-);
+)
 
 -- Customers Table
 CREATE TABLE Customers (
@@ -54,13 +54,13 @@ CREATE TABLE Customers (
     CusLastName VARCHAR(255),
     CusStatusId VARCHAR(255),
     FOREIGN KEY (CusStatusId) REFERENCES CustomerStatus(CusStatusId)
-);
+)
 
 -- MaxBorrowLimits Table
 CREATE TABLE MaxBorrowLimits (
     MaxBorrowLimitId BIGINT PRIMARY KEY,
     MaxBorrowNumber INT
-);
+)
 
 -- Borrows Table
 CREATE TABLE Borrows (
@@ -74,7 +74,7 @@ CREATE TABLE Borrows (
     FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId),
     FOREIGN KEY (StaffId) REFERENCES Staffs(StaffId),
     FOREIGN KEY (MaxBorrowLimitId) REFERENCES MaxBorrowLimits(MaxBorrowLimitId)
-);
+)
 
 -- Books Table
 CREATE TABLE Books (
@@ -87,7 +87,7 @@ CREATE TABLE Books (
     Genre VARCHAR(20),
     FOREIGN KEY (AuthorId) REFERENCES Authors(AuthorId),
     FOREIGN KEY (BookStatusId) REFERENCES BooksStatus(BookStatusId)
-);
+)
 
 -- Returns Table
 CREATE TABLE Returns (
@@ -96,4 +96,13 @@ CREATE TABLE Returns (
     ReturnDate DATE,
     Fine DECIMAL(10, 2),
     FOREIGN KEY (BorrowId) REFERENCES Borrows(BorrowId)
-);
+)
+
+-- Create FinePayments table
+CREATE TABLE FinePayments (
+    PaymentId BIGINT PRIMARY KEY,
+    CustomerId BIGINT,
+    PaymentDate DATE,
+    Amount DECIMAL(10, 2),
+    FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId)
+)
