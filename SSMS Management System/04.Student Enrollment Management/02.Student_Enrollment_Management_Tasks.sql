f98045b1af469846baaa75e4d6dcbe040b8f6fa4
Chaemyoung <chaemyoung2015@gmail.com>
2024-06-26 23:35:45 -0700

    Set up tasks to query for Student Enrollment Sys

---
+++ SSMS Management System/04.Student Enrollment Management/02.Student_Enrollment_Management_Tasks.sql
@@ -0,0 +1,10 @@
+--1) List all students along with their ages and email addresses.
+--2) Retrieve the list of courses along with their start and end dates.
+--3) Find all assessments for a specific student.
+--4) List all instructors and their corresponding courses.
+--5) Retrieve the list of students who have registered for more than a specific number of courses.
+--6) Calculate the total number of students enrolled in each course.
+--7) Find the student with the highest final grade in a specific course.
+--8) List all courses a specific student is registered for.
+--9) Retrieve the details of the assessments for a specific course and their average grades.
+--10) List all instructors who have more than a specific number of courses assigned.
