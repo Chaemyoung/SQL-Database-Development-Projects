d9502cb4214422f625584da6a1a0aefd7e1bcacd
Chaemyoung <chaemyoung2015@gmail.com>
2024-07-22 11:15:57 -0700

    Update by add ArtistID to include as a detail

--- SSMS Management System/07.Art Gallery Management/Art_Gallery_Management_Stored_Procedure/sp_AddNewArtwork.sql
+++ SSMS Management System/07.Art Gallery Management/Art_Gallery_Management_Stored_Procedure/sp_AddNewArtwork.sql
@@ -2,10 +2,11 @@
 CREATE OR ALTER PROCEDURE sp_AddNewArtwork
     @ArtTitle VARCHAR(100),
     @ArtType VARCHAR(50),
-    @ArtMedium VARCHAR(50)
+    @ArtMedium VARCHAR(50),
+    @ArtistID INT
 AS
 BEGIN
-    INSERT INTO Art (Title, Type, Medium)
-    VALUES (@ArtTitle, @ArtType, @ArtMedium)
+    INSERT INTO Art (Title, Type, Medium, ArtistID)
+    VALUES (@ArtTitle, @ArtType, @ArtMedium, @ArtistID)
 END
 GO
