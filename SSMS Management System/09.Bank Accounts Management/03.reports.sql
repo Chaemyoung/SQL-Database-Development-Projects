26056cb65d76def10ed8be7f2e1ba941c8f53957
Chaemyoung <chaemyoung2015@gmail.com>
2024-08-10 21:37:53 -0700

    Update by add 3 more SQL to get useful reports

--- SSMS Management System/09.Bank Accounts Management/03.reports.sql
+++ SSMS Management System/09.Bank Accounts Management/03.reports.sql
@@ -28,3 +28,29 @@ FROM [Transaction]
 WHERE AccountID = @AccountID 
 ORDER BY TransactionDate DESC
 
+
+-- 6) Total Balance for All Accounts by Customer
+SELECT C.CustomerID, C.FirstName + ' ' + C.LastName AS CustomerName, SUM(A.AccountBalance) AS TotalBalance
+FROM Customer C
+INNER JOIN Account A ON C.CustomerID = A.CustomerID
+GROUP BY C.CustomerID, C.FirstName, C.LastName
+ORDER BY TotalBalance DESC
+
+
+-- 7) Transactions in the Last 30 Days
+SELECT T.TransactionID, C.FirstName + ' ' + C.LastName AS CustomerName, A.AccountNumber,
+    T.TransactionDate, T.TransactionType, T.Amount, T.Mode, T.Status
+FROM [Transaction] T
+INNER JOIN Account A ON T.AccountID = A.AccountID
+INNER JOIN  Customer C ON A.CustomerID = C.CustomerID
+WHERE T.TransactionDate >= DATEADD(DAY, -30, GETDATE())
+ORDER BY T.TransactionDate DESC
+
+
+-- 8) Active Credit Cards by Customer
+SELECT C.CustomerID, C.FirstName + ' ' + C.LastName AS CustomerName,
+    Cd.CardNumber, Cd.CardType, Cd.ExpirationDate
+FROM Customer C
+INNER JOIN Card Cd ON C.CustomerID = Cd.CustomerID
+WHERE Cd.IsActive = 1 AND Cd.CardType = 'Credit'
+ORDER BY C.CustomerID, Cd.ExpirationDate
