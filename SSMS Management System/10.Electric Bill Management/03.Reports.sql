5b11981cc893797d5fc61a91398f202e430487b7
Chaemyoung <chaemyoung2015@gmail.com>
2024-08-20 22:43:34 -0700

    Add SQL queries to get 3 meaningful reports

---
+++ SSMS Management System/10.Electric Bill Management/03.Reports.sql
@@ -0,0 +1,29 @@
+-- 1) Monthly Billing Report
+SELECT C.FirstName + ' ' + C.LastName AS CustomerName, B.BillDate,
+    B.UnitsConsumed, B.AmountDue,
+    CASE 
+        WHEN B.IsPaid = 1 THEN 'Paid'
+        ELSE 'Unpaid'
+    END AS PaymentStatus
+FROM Billing B
+INNER JOIN Account A ON B.AccountID = A.AccountID
+INNER JOIN Customer C ON A.CustomerID = C.CustomerID
+WHERE MONTH(B.BillDate) = 8 AND YEAR(B.BillDate) = 2024
+
+
+-- 2) Vendor-Wise Customer Count Report
+SELECT V.VendorName, COUNT(C.CustomerID) AS NumberOfCustomers
+FROM Vendor V
+LEFT JOIN Customer C ON V.VendorID = C.VendorID
+GROUP BY V.VendorName
+ORDER BY NumberOfCustomers DESC
+
+
+-- 3) Top 5 High-Consumption Customers
+SELECT C.FirstName + ' ' + C.LastName AS CustomerName,
+    B.BillDate, B.UnitsConsumed, B.AmountDue
+FROM Billing B
+INNER JOIN Account A ON B.AccountID = A.AccountID
+INNER JOIN Customer C ON A.CustomerID = C.CustomerID
+ORDER BY B.UnitsConsumed DESC
+OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY
